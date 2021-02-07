3b1f8a314ea01b377d1bc125bba500a12ea60cc3
Whateverman2012 <lahos@hotmail.com>
2021-02-06 22:39:13 -0500

    p2p

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-'use strict';
-
 const express = require('express');
-const { Server } = require('ws');
+const { ExpressPeerServer } = require('peer');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const app = express();
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('/', (req, res, next) => res.send('Hello world!'));
 
-const wss = new Server({ server });
+// =======
 
-const { PeerServer } = require('peer');
+const server = app.listen(9000);
 
-const peerServer = PeerServer({ port: 9000, path: '/peer' });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+const peerServer = ExpressPeerServer(server, {
+  path: '/myapp'
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+app.use('/peerjs', peerServer);
