cff5704f6db43f7cedf5b76578ba0f5a42cac8ac
Whateverman2012 <lahos@hotmail.com>
2021-02-07 01:10:13 -0500

    cross

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
 const server = express()
 //  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -12,15 +11,27 @@ const server = express()
 
 const wss = new Server({ server });
 
-
-
 wss.on('connection', (ws) => {
+	
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+	
+	ws.on('message', function incoming(message) {
+//		if message.includes("ui")
+			console.log('received: %s', message);
+			wss.clients.forEach(function(client) {       
+				if (client !== ws) client.send(message);
+			});
+  });
+	
+  ws.on('close', () => {
+		console.log('Client disconnected')
+	});
 });
 
+/*	
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
+*/
